5cac45db78233b95f5d9169c8587ba21ff32b872
Martí Bordoy <estampa075@gmail.com>
2026-05-19 22:48:10 +0200

    Create operacions.sh

---
+++ operacions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#variables són "paraules" que contenen un valor
+NOM="Iker"
+
+#read serveix per llegir informació del teclat
+read -p "introdueix un nom: " NOM
+
+
+echo "El teu nom es ${NOM}"
